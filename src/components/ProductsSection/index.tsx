6bcebd63abf712b2976584126be215cfebf8380f
matheusjesse <matheus_jesse@hotmail.com>
2023-02-14 10:39:29 -0300

    feat: configurando mock de produtos para nao precisar ficar fazendo requisição toda hora na API

--- src/components/ProductsSection/index.tsx
+++ src/components/ProductsSection/index.tsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect } from 'react';
-import fetchProductLimit from 'src/api/getProducts';
+// import fetchProductLimit from 'src/api/getProducts';
 import ProductContext from 'src/context/ProductsContext';
 import { ProductContextType } from 'src/interface/productInterface';
+import productsMock from 'src/mocks/mockProducts';
 import ProductCard from '../ProductCard';
 import ProductSectionContainer from './style'
 
@@ -13,8 +14,8 @@ export function ProductsSection() {
       } = useContext(ProductContext) as ProductContextType;
 
     const fetchProducts = async () => {
-        const data = await fetchProductLimit();
-        productsSave(data);
+        // const data = await fetchProductLimit();
+        productsSave(productsMock);
     }
 
     useEffect(() => {
