8b5f6331f7ec116d908e9ec58eeeba805b329c8c
matheusjesse <matheus_jesse@hotmail.com>
2023-02-15 09:16:53 -0300

    feat: tags html do categoryCard

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -1,9 +1,11 @@
 import CategoryCardContainer from "./style";
 
-export function CategoryCard() {
+export function CategoryCard() {    
+
     return(
         <CategoryCardContainer>
-            
+            <img src="" alt=""></img>
+            <h1></h1>
         </CategoryCardContainer>
     )
 }
