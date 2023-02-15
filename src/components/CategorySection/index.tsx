54dbc5f4dfdcf0bbd26716be9f543452129dae2e
matheusjesse <matheus_jesse@hotmail.com>
2023-02-15 09:21:06 -0300

    feat: criando o category state no section category

--- src/components/CategorySection/index.tsx
+++ src/components/CategorySection/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import { CategoryCard } from "../CategoryCard";
 import CategorySectionContainer from "./style";
 
 export function CategorySection() {
+    
+    const [category, setCategory] = useState([
+        "electronics",
+        "jewelery",
+        "men's clothing",
+        "women's clothing"
+      ]);
+
     return(
         <CategorySectionContainer>
-
+            <CategoryCard 
+                
+            />
         </CategorySectionContainer>
     )
 }
